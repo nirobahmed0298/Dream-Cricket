0875214bcb5d91a0cb6463be9ba1e395ee691e6b
nirobahmed0298 <nirobahmed0298@gmail.com>
2024-10-25 07:24:05 +0600

    set up files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+  return (
+    <div className='w-10/12 mx-auto py-4'>
+        <Header></Header>
+    </div>
+  )
+}
+
+export default App
